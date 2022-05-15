9851974930f28f00d72bad02a90a90096b3f8809
Mooteus <moteus10@gmail.com>
2022-05-15 09:36:13 -0300

    Test: check if the redirects are correct

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+
+const LINK_TEXTS = ['Home', 'About', 'Favorite Pokémons'];
+const LINK_PATH = ['/', '/about', '/favorites'];
 
 describe('Test App.js', () => {
   test('01- Checks if the links have the correct text', () => {
     renderWithRouter(<App />);
-    const TEXTS = ['Home', 'About', 'Favorite Pokémons'];
-    const LINK_TEST = screen.getAllByRole('link');
-    TEXTS.forEach((text, i) => {
-      expect(LINK_TEST[i]).toHaveTextContent(text);
+    const LINKS = screen.getAllByRole('link');
+    LINK_TEXTS.forEach((text, i) => {
+      expect(LINKS[i]).toHaveTextContent(text);
+    });
+  });
+
+  LINK_TEXTS.forEach((text, i) => {
+    test(`0${i + 2}- Check if the ${text} redict is correct`, () => {
+      const { history } = renderWithRouter(<App />);
+      const LINK = screen.getByRole('link', { name: text });
+      userEvent.click(LINK);
+      const { pathname } = history.location;
+      expect(pathname).toBe(LINK_PATH[i]);
     });
   });
 });
