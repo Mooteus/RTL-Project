b7a1e7bec0b6861f4ddb2a07e224c9c7372cbef6
Mooteus <moteus10@gmail.com>
2022-05-15 09:05:19 -0300

    Test: Check if the links have the correct text

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Test App.js', () => {
+  test('01- Checks if the links have the correct text', () => {
+    renderWithRouter(<App />);
+    const TEXTS = ['Home', 'About', 'Favorite Pokémons'];
+    const LINK_TEST = screen.getAllByRole('link');
+    TEXTS.forEach((text, i) => {
+      expect(LINK_TEST[i]).toHaveTextContent(text);
+    });
+  });
+});
