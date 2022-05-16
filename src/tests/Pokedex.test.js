74a642ce37e864870c3d0cf9cbcb32fc327ce732
Mooteus <moteus10@gmail.com>
2022-05-16 08:02:56 -0300

    Test: Checks if the component has the correct title

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+
+const favoritedPokemons = pokemons.reduce((acc, { id }) => {
+  acc[id] = false;
+  return acc;
+}, {});
+
+describe('Test Pokedex', () => {
+  test('01- Checks if the component has the correct title', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ favoritedPokemons }
+    />);
+    const title = screen.getByRole('heading',
+      { level: 2, name: /Encountered pokémons/i });
+    expect(title).toBeInTheDocument();
+  });
+});
