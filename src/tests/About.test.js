c78b99e2ce8f8f0cfc0e6229e161dbf06332725e
Mooteus <moteus10@gmail.com>
2022-05-16 05:02:44 -0300

    Test: H2 text

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Test About', () => {
+  test('01- Checks if it has an "h2" with the text "About Pokédex"', () => {
+    renderWithRouter(<About />);
+    const title = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(title).toBeInTheDocument();
+  });
+});
