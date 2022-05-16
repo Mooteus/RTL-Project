6edcae045982cf7bb1c2585fc42169e9f7f53a1a
Mooteus <moteus10@gmail.com>
2022-05-16 06:58:05 -0300

    Test: Checks if the page shows the correct image

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -10,4 +10,12 @@ describe('Test NotFound', () => {
       { level: 2, name: /Page requested not found/i });
     expect(notFoundText).toBeInTheDocument();
   });
+
+  test('02- Checks if the page shows the correct image', () => {
+    renderWithRouter(<NotFound />);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const images = screen.getAllByRole('img');
+    const imagesFind = images.some(({ src }) => src === url);
+    expect(imagesFind).toBe(true);
+  });
 });
