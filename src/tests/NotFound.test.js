4da1f53f52a26cb7510d9f09edd7d038dd3fa254
Mooteus <moteus10@gmail.com>
2022-05-16 06:47:59 -0300

    Test: Checks if the page has the text of not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Test NotFound', () => {
+  test('01- Checks if the page has the text of not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundText = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
