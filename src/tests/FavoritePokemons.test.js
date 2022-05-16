bb1e798593ca3aa88fa0f7e4a727c457fffe48a2
Mooteus <moteus10@gmail.com>
2022-05-16 05:13:27 -0300

    Test: not favorite pokemon found

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Test FavoritePokemons', () => {
+  test('01- Checks if it displays a message "no favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFountText = screen.getByText(/No favorite pokemon found/i);
+    expect(notFountText).toBeInTheDocument();
+  });
+});
