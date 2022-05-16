e91b1f43533076d9d25b6fe65e10610d3f1224f8
Mooteus <moteus10@gmail.com>
2022-05-16 05:03:32 -0300

    Test: Checks if the page contains two paragraphs

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,4 +9,14 @@ describe('Test About', () => {
     const title = screen.getByRole('heading', { name: /about pokédex/i });
     expect(title).toBeInTheDocument();
   });
+
+  test('02- Checks if the page contains two paragraphs', () => {
+    renderWithRouter(<About />);
+    const paragraphs = [];
+    paragraphs[0] = screen.getByText(/This application simulates a Pokédex/i);
+    paragraphs[1] = screen.getByText(/One can filter Pokémons by type/i);
+    paragraphs.forEach((paragraph) => {
+      expect(paragraph).toBeInTheDocument();
+    });
+  });
 });
