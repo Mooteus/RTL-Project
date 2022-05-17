65196bc58147786259df2a1b4ad3412049ad5020
Mooteus <moteus10@gmail.com>
2022-05-17 09:13:42 -0300

    Test: Checks if the detailed information of pokemon is shown

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import data from '../data';
+
+describe('Test PokemonDetais', () => {
+  test('01- Checks if the detailed information of pokemon is shown', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/10');
+
+    const pokeName = screen.getByText(/caterpie details/i);
+    const linkDetails = screen.queryByRole('link', { name: /more details/i });
+    const detailsTitle = screen.getByRole('heading', { level: 2, name: /summary/i });
+    const details = screen.getByText(data[2].summary);
+
+    expect(linkDetails).not.toBeInTheDocument();
+    expect(details && detailsTitle && pokeName).toBeInTheDocument();
+  });
+});
